d2429c9f18a1b4a42e90244c6a1d3c0456df8937
liamholland <liamholland02@gmail.com>
2023-05-18 13:00:44 +0100

    initial commit

---
+++ app/src/main/java/com/example/tangeryme/App.kt
@@ -0,0 +1,11 @@
+package com.example.tangeryme
+
+//main class for the application
+//this class should check for a new prompt on app startup and act accordingly
+//i.e. if there is a new prompt, make the user try it, otherwise display the previous prompts
+class App {
+
+    fun main(){
+
+    }
+}
