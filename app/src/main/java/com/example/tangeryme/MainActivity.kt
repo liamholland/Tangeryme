28abf2709d59a74546b8f72f8ce7e0597574abce
liamholland <liamholland02@gmail.com>
2023-05-18 13:23:45 +0100

    added the previous page layout

--- app/src/main/java/com/example/tangeryme/MainActivity.kt
+++ app/src/main/java/com/example/tangeryme/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var newPromptAvailable: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.poem_submission)
+        if(newPromptAvailable){
+            setContentView(R.layout.poem_submission)
+        }
+        else{
+            setContentView(R.layout.previous_poems)
+        }
     }
 }
